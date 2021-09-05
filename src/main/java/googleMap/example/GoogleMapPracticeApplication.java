4b6bb7015edea570d71d7439f9ce4994f3bafe1c
tosumaso <wakuwaku5712@gmail.com>
2021-09-05 18:15:42 +0900

    first commit

---
+++ src/main/java/googleMap/example/GoogleMapPracticeApplication.java
@@ -0,0 +1,13 @@
+package googleMap.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/googleMap/example/GoogleMapPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package googleMap.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMapPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
